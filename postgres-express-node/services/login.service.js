d80b17e4dd137e131870472248f0c39e7a3f48ff
ajuric04 <ajuric04@fesb.hr>
2020-12-02 18:31:57 +0100

    status

--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -1,3 +1,6 @@
+
+const jmt = require("jsonwebtoken");
+const config = require("../config");
 class LoginService {
   constructor({ logger, userModel }) {
     this.userModel = userModel;
@@ -10,6 +13,39 @@ class LoginService {
     });
     return user;
   }
+
+  async login({username, password}){
+    const userRecord = await this.userModel.findOne({
+      where: { username },
+    });
+    if (!userRecord){
+      this.logger.error("user not registered");
+      throw new error("authentication failed");
+    }
+    this.logger.info("checking password");
+    if(userRecord.password === password){
+      this.logger.info("password correct");
+      const user = {
+        username: userRecord.username,
+        role: userRecord.role || "guest",
+      };
+      const payload = {
+        ...user,
+        aud: config.jwt.audience || "localhost/api",
+        iss: config.jwt.issuer || "localhost@fesb",
+      };
+      const token = this.generateToken(payload);
+
+      return {user, token}
+
+    }
+  }
+generateToken(payload){
+  return jwt.sign(payload, config.jwt.secret, {
+    expiresIn: config.jwt.expiresIn,
+  });
+}
+
 }
 
 module.exports = LoginService;
