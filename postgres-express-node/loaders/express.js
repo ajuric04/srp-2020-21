a62a95384fa2462fd98b34c54301c3899f89e5e5
ajuric04 <ajuric04@fesb.hr>
2020-12-12 12:41:43 +0100

    status

--- postgres-express-node/loaders/express.js
+++ postgres-express-node/loaders/express.js
@@ -14,7 +14,7 @@ module.exports = ({ app, HttpLogger: logger }) => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  app.use(jwz({
+  app.use(jwt({
     algorithms: config.jwt.algorithms,
     secret: config.jwt.secret,
   }).unless(config.jwt.exclude)
